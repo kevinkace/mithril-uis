51b50e636968e06209247242e0ac8e38c20521b6
kevinkace <kevinacameron@hotmail.com>
2016-11-01 08:48:43 -0700

    Initial commit with 360

---
+++ public/360/js/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let currStep = m.prop(0);
+
+// setInterval(() => console.log(currStep()), 500);
+
+function bgp(currStep, steps, frames) {
+    let div = 100 / (frames - 1),
+        per = (currStep/steps) * 100 + div / 2,
+        num = Math.floor(per / div) * div;
+
+    return `${num}% 0`;
+}
+
+const frames = 10,
+    steps    = 50,
+    comp     = {
+        view : (ctrl) => {
+            return m("div", { class : "container" },
+                m("div", {
+                    class : "viewer",
+                    style : {
+                        backgroundImage : "url(/img/1-0.png)",
+                        backgroundPosition : bgp(currStep(), steps, frames)
+                    }
+                }),
+                m("input", {
+                    class   : "range",
+                    type    : "range",
+                    min     : 0,
+                    max     : steps,
+                    step    : 1,
+                    oninput : m.withAttr("value", currStep),
+                    value   : currStep()
+                })
+            );
+        }
+    };
+
+m.mount(document.body, comp);
